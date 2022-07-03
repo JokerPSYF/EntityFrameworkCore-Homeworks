3ae03a8665b6fd6b951bb3e7036080c04d57b7fc
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-07-03 14:59:35 +0300

    AdoNet add

---
+++ AdoNetExcircises/AdoNetExcircises/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExcircises
+{
+    public class Config
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-EFGIFFL\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+    }
+}
