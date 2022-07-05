714180de98858d271ce92ad0a5917a07f730bc43
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-07-06 00:37:40 +0300

    relation added in P03

---
+++ EntityRelations/P03_FootballBetting.Data.Models/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using P03_FootballBetting.Data.Models.Enum;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Game
+    {
+        public Game()
+        {
+            this.Bets = new HashSet<Bet>();
+            this.PlayerStatistics = new HashSet<PlayerStatistic>();
+        }
+
+        [Key]
+        public int GameId { get; set; }
+
+        [ForeignKey(nameof(Team))]
+        public int HomeTeamId { get; set; }
+        public virtual Team HomeTeam { get; set; }
+
+        [ForeignKey(nameof(Team))]
+        public int AwayTeamId { get; set; }
+        public virtual Team AwayTeam { get; set; }
+
+        public byte HomeTeamGoals { get; set; }
+
+        public byte AwayTeamGoals { get; set; }
+
+        public DateTime? DateTime { get; set; }
+
+        public decimal HomeTeamBetRate { get; set; }
+
+        public decimal AwayTeamBetRate { get; set; }
+
+        public decimal DrawBetRate { get; set; }
+
+        public Prediction Result { get; set; }
+
+        public virtual ICollection<Bet> Bets { get; set; }
+
+        public virtual ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+
+    }
+}
---
+++ EntityRelations/P03_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Bets = new HashSet<Bet>();
+        }
+
+        [Key]
+        public int UserId { get; set; }
+
+        [MaxLength(30)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Password { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Bet> Bets { get; set; }
+    }
+}
