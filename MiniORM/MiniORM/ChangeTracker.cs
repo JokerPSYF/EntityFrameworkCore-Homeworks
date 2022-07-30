f4ea37581f7e8c4c09ed45402b7bc04b1f6f4149
JokerPSYF <todorvasilev005@gmail.com>
2022-07-30 18:25:54 +0300

    Mini ORM skeleton added still in work

---
+++ MiniORM/MiniORM/ChangeTracker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MiniORM
+{
+	internal class ChangeTracker<T> where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+    }
+}
