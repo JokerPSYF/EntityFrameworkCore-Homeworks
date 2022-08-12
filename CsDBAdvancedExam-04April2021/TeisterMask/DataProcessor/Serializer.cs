fc4d5a93227c13bb44ca809295bd9860f1a8bde5
JokerPSYF <todorvasilev005@gmail.com>
2022-08-12 22:16:01 +0300

    12.08.22 complete it

--- CsDBAdvancedExam-04April2021/TeisterMask/DataProcessor/Serializer.cs
+++ CsDBAdvancedExam-04April2021/TeisterMask/DataProcessor/Serializer.cs
@@ -4,18 +4,91 @@
 
     using Data;
 
+    using System.Linq;
+
     using Formatting = Newtonsoft.Json.Formatting;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Xml.Serialization;
+    using System.Text;
+    using System.IO;
+    using TeisterMask.DataProcessor.ExportDto;
+    using Castle.Core.Internal;
 
     public class Serializer
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
-            throw new NotImplementedException();
+            ExportProjectDTO[] proj = context.Projects
+                .Where(p => p.Tasks.Any())
+                .ToArray()
+                .Select(p => new ExportProjectDTO
+                {
+                    TasksCount = p.Tasks.Count,
+                    ProjectName = p.Name,
+                    HasEndDate = p.DueDate != null ? "Yes" : "No",
+                    Tasks = p.Tasks
+                        .Select(t => new ExportTasksDTO
+                        {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                        })
+                        .OrderBy(t => t.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(x => x.TasksCount)
+                .ThenBy(y => y.ProjectName)
+                .ToArray();
+
+            return Serialize(proj, "Projects");
         }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var emp = context.Employees
+                .Include(c => c.EmployeesTasks)
+                .ThenInclude(et => et.Task)
+                .ToArray()
+                .Where(x => x.EmployeesTasks.Any(t => t.Task.OpenDate >= date))
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                    .Where(x => x.Task.OpenDate >= date)
+                    .OrderByDescending(x => x.Task.DueDate)
+                    .ThenBy(y => y.Task.Name)
+                    .Select(t => new
+                    {
+                        TaskName = t.Task.Name,
+                        OpenDate = t.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate =  t.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = t.Task.LabelType.ToString(),
+                        ExecutionType = t.Task.ExecutionType.ToString()
+                    })
+                })
+                .OrderByDescending(x => x.Tasks.Count())
+                .ThenBy(y => y.Username)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(emp, Formatting.Indented);
+        }
+
+        private static string Serialize<T>(T dto, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, dto, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
